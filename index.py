bfd64cb109db6f11cba2da429675af59dcb95e5e
Apoorv Garg <u.garg.10@gmail.com>
2020-12-14 21:50:57 +0530

    Commit

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 from flask import Flask,session,render_template,redirect,url_for
-from modules.get_key import get_key
 from modules.database import mongo
 from decouple import config
 
@@ -15,28 +14,6 @@ def home():
         session['fuzzy']=float(get_key())
 
     return render_template(str(session['fuzzy'])+"/"+"index.html")
-
-@app.route('/set/<key>')
-def set_key(key):
-    try:
-        key=float(key)
-    except:
-        key=5
-
-    session['fuzzy']=key
-    
-    return redirect(url_for("home"))
-
-@app.route('/pop')
-def pop():
-    session.pop('fuzzy',None)
-    return redirect(url_for("home"))
-
-@app.route('/check')
-def check():
-    print("\n\nSession Key: "+session['fuzzy'])
-    return redirect(url_for("home"))
-
 
 def main():
     app.run(debug=True)
