16c6b82814c9fd23c5bee123950c5903ddf37e0a
Apoorv Garg <u.garg.10@gmail.com>
2020-12-14 22:56:41 +0530

    Update index.py

--- index.py
+++ index.py
@@ -38,7 +38,7 @@ def view():
     return render_template("list.html",items=items)
 
 def main():
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
 
 if __name__=='__main__':
     main()
