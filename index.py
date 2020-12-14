b859137513d4f89e38aa823b8cd08a90d9559e55
Apoorv Garg <u.garg.10@gmail.com>
2020-12-14 22:54:20 +0530

    Data Entry

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask,session,render_template,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for
 from modules.database import mongo
 from decouple import config
 
@@ -10,10 +10,32 @@ app.secret_key=config('SECRET_KEY')
 
 @app.route("/")
 def home():
-    if 'fuzzy' not in session:
-        session['fuzzy']=float(get_key())
+    return render_template("index.html")
 
-    return render_template(str(session['fuzzy'])+"/"+"index.html")
+@app.route("/add",methods=['POST'])
+def add():
+    name=request.form.get('name')
+    description=request.form.get('description')
+    image=request.form.get('image')
+    key=request.form.get('key')
+
+    fashion_collection=mongo.db.fashion
+
+    fashion_collection.insert_one({
+        'name':name,
+        'description':description,
+        'image':image,
+        'key':key,
+    })
+
+    return redirect(url_for('home'))
+
+@app.route("/view")
+def view():
+    fashion_collection=mongo.db.fashion
+    items=fashion_collection.find()
+
+    return render_template("list.html",items=items)
 
 def main():
     app.run(debug=True)
